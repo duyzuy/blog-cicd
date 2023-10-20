97d35f764789af74ed76e8c3ba75325a9804ee95
vu truong duy <vutruongduy2109@gmail.com>
2023-10-20 22:52:15 +0700

    update

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,15 +1,13 @@
 import { render, screen } from '@testing-library/react'
 import Homepage from '../pages/index'
+import { getAllPosts } from '../lib/api'
 import '@testing-library/jest-dom'
  
 describe('Home', () => {
-  it('renders a blog', () => {
-    render(<Homepage />)
- 
-    const heading = screen.getByRole('heading', {
-      name: /welcome to next\.js!/i,
-    })
- 
-    expect(heading).toBeInTheDocument()
-  })
+    test('the data is peanut butter', () => {
+        return getAllPosts(["title", "date", "slug", "author", "coverImage", "excerpt"]).then(data => {
+          expect(data).toBeDefined();
+        });
+      });
 })
+
