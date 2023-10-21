c1cb2a436872668d4cca3a3bb63c073c3ea1ed0d
vu truong duy <vutruongduy2109@gmail.com>
2023-10-22 02:50:49 +0700

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,7 +21,7 @@ export default function Index({ allPosts }: Props) {
         <Head>
           <title>{`Next.js Blog Example with ${CMS_NAME}`}</title>
         </Head>
-        <p>update content then build - update</p>
+        <p>update content then build - update again</p>
         <Container>
           <Intro />
           {heroPost && (
